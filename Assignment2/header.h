e0330ea4b16dbbc9f4930f7216f8752ecdf246a4
acbikash13 <96610618+acbikash13@users.noreply.github.com>
2023-12-10 15:23:58 -0500

    Update header.h

--- Assignment2/header.h
+++ Assignment2/header.h
@@ -1,25 +1,27 @@
+#ifndef HEADER_H
+#define HEADER_H
 
-#define _CRT_SECURE_NO_WARNINGS
-// inlcude statements
 #include <stdio.h>
 #include <math.h>
 
-
-// Defining Constants
 #define OFFENSIVE_FACTOR 1.15
 #define SPECIAL_TEAMS_FACTOR 1.22
 #define HOME_FIELD_ADVANTAGE 1.2
 #define OVERALL_FACTOR 1.15
-#define OFFENSIVE_WEIGHT 0.3
+#define OFFENSIVE_WEIGHT 0.30
 #define DEFENSIVE_WEIGHT 0.24
+#define SPECIAL_WEIGHT 0.17
 #define HOME_WEIGHT 0.11
 #define OVERALL_WORTH 0.13
 
+int getInput(FILE *file, char *homeTeam, int *HTO, int *HTD, int *HTS, int *HTH, int *HTC,
+             char *visitingTeam, int *VTO, int *VTD, int *VTS, int *VTR);
+void updateTotals(int *totalGames, int *homeWins, int *totalDifference, int currentPrediction);
+double computeScoreDifference(int HTO, int HTD, int HTS, int HTH, int HTC,
+                              int VTO, int VTD, int VTS, int VTR);
+void outputResult(char *winner, char *loser, int pointDiff);
+void computeSummary(int totalGames, int homeWins, int totalDifference,
+                    double *homeWinPercentage, double *avgScoreDifference);
+void outputSummary(double homeWinPercentage, double avgScoreDifference);
 
-// prototypes
-int getInput();
-void computeStrengths();
-int computeScoreDifference();
-void outputResult();
-void updateTotals();
-void summary();
+#endif // HEADER_H
